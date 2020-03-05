8258fbe55952e5a7b32dc9625d03a6962041eacb
Emerson da Silva Coutinho <emerson.coutinho@meta.com.br>
2020-03-05 17:35:43 -0300

    initial commit

---
+++ src/common/store/index.js
@@ -0,0 +1,24 @@
+import { persistStore } from 'redux-persist';
+import createSagaMiddleware from 'redux-saga';
+import createStore from './createStore';
+import history from "./history";
+import persistReducer from './persistReducer';
+import '../../config/ReactotronConfig';
+import reducers from './reducers';
+import sagas from './sagas';
+
+const sagaMonitor =
+  process.env.NODE_ENV === 'development'
+    ? console.tron.createSagaMonitor()
+    : null;
+
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+
+const middlewares = [sagaMiddleware];
+
+const store = createStore(persistReducer(reducers(history)), middlewares);
+const persistor = persistStore(store);
+
+sagaMiddleware.run(sagas);
+
+export { store, persistor };
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import { ConnectedRouter } from "connected-react-router";
+import history from "./common/store/history";
+import Dashboard from  './app/nomeApp/dashboard';
+
+
+export default function Routes(){
+  return(
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/" exact component={Dashboard}/>
+      </Switch>
+    </ConnectedRouter>
+  )
+}
